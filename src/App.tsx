99c0d8db0ce03990dc2eae0bfb3de4cf550e1398
André Silva <andre.silva@posteo.de>
2023-07-14 10:22:52 +0200

    Add form layout and state

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Todo App Tutorial</h1>
-    </div>
+   
+   <div>
+
+    <Toaster position="bottom-center"/>
+    <AddTodo/>
+    <TodoList/>
+   </div>
   )
 }
 
