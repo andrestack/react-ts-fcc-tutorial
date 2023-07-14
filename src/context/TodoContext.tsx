2b1cea003a40ab6a518d04c7cc201ba6bbb6d661
André Silva <andre.silva@posteo.de>
2023-07-14 16:22:15 +0200

    Complete UI and handle functions

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -5,6 +5,9 @@ import { useLocalStorage } from 'usehooks-ts'
 interface TodoContextProps {
   todos: Todo[]
   addTodo: (text: string) => void
+  deleteTodo: (id: string) => void
+  editTodo: (id: string, text: string) => void
+  updateTodoStatus: (id: string) => void
 }
 
 export interface Todo {
@@ -18,7 +21,9 @@ export const TodoContext = createContext<TodoContextProps | undefined>(
 )
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', [])
+
+  // ADD NEW TODO
   const addTodo = (text: string) => {
     const newTodo: Todo = {
       id: nanoid(),
@@ -29,9 +34,47 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
     setTodos([...todos, newTodo])
   }
 
+  // DELETE A TODO
+
+  const deleteTodo = (id: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  // EDIT A TODO
+
+  const editTodo = (id: string, text: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, text }
+        }
+        return todo
+      })
+    })
+  }
+
+  // UPDATE TODO STATUS
+
+  const updateTodoStatus = (id: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            status: todo.status === 'undone' ? 'completed' : 'undone',
+          }
+        }
+        return todo
+      })
+    })
+  }
+
   const value: TodoContextProps = {
     todos,
     addTodo,
+    deleteTodo,
+    editTodo,
+    updateTodoStatus,
   }
 
   return (
